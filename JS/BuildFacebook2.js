f3a6eac33a74f916e194b95a57ee2c920221f622
RaeganTysh <rae-gan@shaw.ca>
2021-01-06 12:40:28 -0700

    updates

--- JS/BuildFacebook2.js
+++ JS/BuildFacebook2.js
@@ -1,5 +1,5 @@
 /*user need to be in the database*/
-var database =[
+var database = [
     {
         username: "Raegan",
         password: "1234"
@@ -20,22 +20,44 @@ var newsfeed = [
         tiemeLine: "So tired from all that javascript learning"
     },
     {
-      username: "Frank",
-      timeLine: "Js is so cool"  
+        username: "Frank",
+        timeLine: "Js is so cool"
     }
 ];
 
 var userNamePrompt = prompt("what's you username");
 var passwordPrompt = prompt("what's your password?");
 
-function signIn(user, pass) {
-    for (var i=0; i < database.length; i++) {
-        if(database[i].username === user && 
-            database[i].password === pass){
-                console.log(newsfeed);
-        }else {
+/*userNamePrompt/passwordPrompt are the arguments for user and pass in isUserValid &signIn function*/
+
+function isUserValid(user, pass) {
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].username === user &&
+            database[i].password === pass) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*function signIn(user, pass) {
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].username === user &&
+            database[i].password === pass) {
+            console.log(newsfeed); tess
+        } else {
             alert("Sorry, wrong username and password");
         }
+    }
+}*/
+
+/* will show true or false in console log*/
+function signIn(user, pass) {
+    /*console.log(isUserValid(user, pass));*/
+    if (isUserValid(user, pass)) {
+        console.log(newsfeed);
+    }else {
+        alert("Sorry, wrong username and password");
     }
 }
 
