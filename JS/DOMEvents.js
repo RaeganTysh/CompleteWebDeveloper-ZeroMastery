5bd3fa6febc012d7fcab29b9590c7c8a71344310
RaeganTysh <rae-gan@shaw.ca>
2021-01-06 14:15:15 -0700

    DOM Updates

--- JS/DOMEvents.js
+++ JS/DOMEvents.js
@@ -1,22 +1,39 @@
 /*remember this accesses an array not the element*/
+/*Keyboard and mouse events ex)click /mouse enter/leave/refresh...ect*/
 var button1 = document.getElementsByTagName("button")[1];
 button1.addEventListener("click", function() {
 	console.log("CLICK!!!");
 })
 
+/*select the button and input tags*/
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+
 var ul = document.querySelector("ul");
+
+/*addEventListner and test if working */
+button.addEventListener("click", function() {
+	console.log(input.value);
+	console.log("click is working!!!");
+})
+
+
+
+
+
 
 function inputLength() {
 	return input.value.length;
 }
 
+/*add text to bottom of list */
 function createListElement() {
+	if(input.value.length > 0) {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	}
 }
 
 function addListAfterClick() {
