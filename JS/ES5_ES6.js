e4cd7530bd093cf9bb97209031e36ba737a79d81
RaeganTysh <rae-gan@shaw.ca>
2021-01-23 12:53:19 -0700

    updates

--- JS/ES5_ES6.js
+++ JS/ES5_ES6.js
@@ -150,3 +150,97 @@ const obj = {
 
 add(4,2);
 add2(4,2);
+
+//Excersizes change everything below to the newer Javascript!
+
+// let + const
+var a = 'test';
+var b = true;
+var c = 789;
+a = 'test2';
+//new JS
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+// Destructuring
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+//new JS
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+const {firstName, lastname, age, eyeColor} = person;
+
+
+// Object properties
+var a = 'test';
+var b = true;
+var c = 789;
+
+var okObj = {
+  a: a,
+  b: b,
+  c: c
+};
+
+//new JS
+const a = 'test';
+const b = true;
+const c = 789;
+
+var okObj = {a, b, c};
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+
+//new JS
+const message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
+
+// default arguments
+// default age to 10;
+function isValidAge(age) {
+    return age
+}
+
+//new JS
+const isValidAge =(age=10) => age; 
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+const sym =Symbol('this is my first Symbol');
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+//new JS
+const whereAmI = (username, location) => {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+
+}
