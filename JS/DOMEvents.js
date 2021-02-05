f84e9dd1bff07443595de56a2298c9df0ddc56ae
RaeganTysh <rae-gan@shaw.ca>
2021-02-05 12:45:37 -0700

    Updates

--- JS/DOMEvents.js
+++ JS/DOMEvents.js
@@ -1,7 +1,7 @@
 /*remember this accesses an array not the element*/
 /*Keyboard and mouse events ex)click /mouse enter/leave/refresh...ect*/
 var button1 = document.getElementsByTagName("button")[1];
-button1.addEventListener("click", function() {
+button1.addEventListener("click", function () {
 	console.log("CLICK!!!");
 })
 
@@ -12,7 +12,7 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 /*addEventListner and test if working */
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
 	console.log(input.value);
 	console.log("click is working!!!");
 })
@@ -30,11 +30,20 @@ function inputLength() {
 
 /*add input value to text to bottom of list */
 function createListElement() {
-	if(input.value.length > 0) {				//if input is 0 do nothing
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";					//clears the inuput value 
+	if (input.value.length > 0) {				//if input is 0 do nothing
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";					//clears the inuput value 
+	}
+}
+
+function createListElement() {
+	if (input.value.length > 0) {				//if input is 0 do nothing
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";					//clears the inuput value 
 	}
 }
 
@@ -50,6 +59,33 @@ function addListAfterKeypress(event) {
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+//button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress);
+//input.addEventListener("keypress", addListAfterKeypress);
+
+
+//adds event listener to
+button.addEventListener("click", function () {
+	if (input.value.length > 0) {				//if input is 0 do nothing
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";					//clears the inuput value 
+	}
+})
+
+
+//add event liststen to enter key
+input.addEventListener("keypress", function() {
+	//console.log(event);	
+	//event.which depreciated //when user presses key give all the stats in console under "KeyBoard Event"	- keyCode 13 therefore
+	//if (input.value.length > 0 && event.keyCode === 13); {		//keyCode depreciated 
+	if (input.value.length > 0) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";					//clears the inuput value 
+	}
+})
+
+//keycode and event no longer recomended
