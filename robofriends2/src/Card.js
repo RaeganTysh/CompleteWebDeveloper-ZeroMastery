af9a00525c09e1031c1ed015da6d239b4cdce3a7
RaeganTysh <rae-gan@shaw.ca>
2021-02-22 13:55:14 -0700

    Robofriends2

--- robofriends2/src/Card.js
+++ robofriends2/src/Card.js
@@ -19,7 +19,7 @@ import React from 'react';
 
 
 //this way with destructuring also works
-const Card = (props) => {
+/*const Card = (props) => {
     const {name, email, id} = props;
     return (
                <div className= 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
@@ -33,6 +33,20 @@ const Card = (props) => {
 
         </div>
     );
-}
+}*/
 
+const Card = ({name, email, id}) => {
+        return (
+               <div className= 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt ='robots' src={`https://robohash.org/${id}?200x200`} />
+            
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+
+
+        </div>
+    );
+}
 export default Card;
