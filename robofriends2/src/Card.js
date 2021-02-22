0bf101f4910ab8c644381bca1407fe554363f13f
RaeganTysh <rae-gan@shaw.ca>
2021-02-22 13:48:49 -0700

    Robofriends2

--- robofriends2/src/Card.js
+++ robofriends2/src/Card.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const Card = () => {
+//this way works 
+/*const Card = (props) => {
     return (
         //<h1></h1>remeber can only return one element
-        <div className= 'bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt ='robots' src='https://robohash.org/test?200x200' />
+        <div className= 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt ='robots' src={`https://robohash.org/${props.id}?200x200`} />
+            
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
+            </div>
+
+
+        </div>
+    );
+}*/
+
+
+//this way with destructuring also works
+const Card = (props) => {
+    const {name, email, id} = props;
+    return (
+               <div className= 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt ='robots' src={`https://robohash.org/${id}?200x200`} />
+            
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
 
 
