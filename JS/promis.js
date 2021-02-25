94cce4058cf0d2029c8473f2118e8bbd1320a8c0
RaeganTysh <rae-gan@shaw.ca>
2021-02-25 12:46:41 -0700

    update promises

--- JS/promis.js
+++ JS/promis.js
@@ -6,5 +6,22 @@ const promise = new Promise((resolve, reject) => {
     }
 })
 
-promise.then(result => console.log(result));
+promise.then(result => console.log(result));       //Stuff Worked
 promise;        //Stuff Worked
+
+
+//chaining in promises
+promise
+    .then(result => result + '!!')
+    .then(result2 => {
+        console.log(result2);
+    })                  //Stuff worked!!
+
+//Error/ Catch
+promise
+    .then(result => result + '!!')
+    .then(result2 => {
+        throw Error
+        console.log(result2);
+    })                  
+    .catch(() => console.log("Errrrroooor!"));
