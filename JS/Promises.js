4bf858c553df441bed49e75483b22b4e168efc60
RaeganTysh <rae-gan@shaw.ca>
2021-03-03 11:55:50 -0700

    Async Await- Finally

--- JS/Promises.js
+++ JS/Promises.js
@@ -131,15 +131,15 @@ Promise6.reject('failed')
 //NOt working - Mixed Content: The page at 'https://jsonplaceholder.typicode.com/users' was loaded over HTTPS, but requested an insecure resource 'http://swapi.dev/api/people/1/'. This request has been blocked; the content must be served over HTTPS.
 
 const urls = [
-  'https://swapi.dev/api/people/1',
-  'https://swapi.dev/api/people/2',
-  'https://swapi.dev/api/people/3',
-  'https://swapi.dev/api/people/4'
+  'https://swapi.co/api/people/1',
+  'https://swapi.co/api/people/2',
+  'https://swapi.co/api/people/3',
+  'https://swapi.co/api/people/4'
 ]
 
-Promise.all(urls.map(url =>
-    fetch(url).then(people => people.json())   ////net::ERR_FAILED- doesn't like this line
-))
+Promise.all(urls.map(url => {
+    return fetch(url).then(people => people.json())   
+}))
   .then(array => {
     console.log('1', array[0])
     console.log('2', array[1])
